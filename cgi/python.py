3c5cdf997d4aa104977e0eacbbafa0b77dcab310
bomanviktor <boman.viktor@gmail.com>
2024-01-10 18:54:15 +0100

    Remove `head` from `cgi` scripts

--- cgi/python.py
+++ cgi/python.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-
-# Print the CGI headers
-print("HTTP/1.1 200")
-print("Host: 127.0.0.1")
-print("Content-type: text/html\n")
-
 # Print the HTML content with "Hello World" and specified font-family
 print("<html><head><style>")
 print("body {font-family: sans-serif; display: flex; align-items: center; justify-content: center; height: 100vh; margin: 0;}")
